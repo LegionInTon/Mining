1da9b36bd17a0f3b475dd704351baf4da5c71fe6
LegionInTon <123652181+LegionInTon@users.noreply.github.com>
2024-02-09 21:37:19 +0200

    Add files via upload

---
+++ givers.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.givers1000 = exports.givers100 = void 0;
+exports.givers100 = [
+    { address: 'EQCBB90ecx2p29nMJQgRDMc8dpe5wJwfcoRjUMiALDQNlqWz', reward: 100000 },
+    { address: 'EQBM8I1p5PEJSgQQqKDaOxuuQzHFp7GLcBd5ChKNaQ159g6S', reward: 100000 },
+    { address: 'EQBEzPiNQlUK0HSHgFnjBq-HDA5xk5_JBAAGudvevPlKGnah', reward: 100000 },
+    { address: 'EQC2udTftic-QAXcsy-GTbiRQqMyEXlhwRL9PFDJZbVQpZfS', reward: 100000 },
+    { address: 'EQDuRwowi_uFUPk_M9wwGev7YRWK1TrTTp5RpuzZqYVjWeSw', reward: 100000 },
+    { address: 'EQBbXnon2BymX5QaJ4c-wBiqCQLufYx7QGkz2mqwM43SZHmI', reward: 100000 },
+    { address: 'EQAEDPdzi4bSb3fBwqWzI88sgkLeOa42hOgesxzeIfbJPtYP', reward: 100000 },
+    { address: 'EQDoMW51I0vaQisLJpi2Kbj8zaN79TgyYM2PnyXr-x80Er5_', reward: 100000 },
+    { address: 'EQCHKWlqed_fOFoZ-TjuhpAXrPE8yg2jnE63gxpZDt8v8WzJ', reward: 100000 },
+    { address: 'EQCTzNRQgFqfTa20MBvepeRpOzzBmnEWwVCII4rEqQw1ufzr', reward: 100000 }, // 100
+];
+exports.givers1000 = [
+    { address: 'EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2', reward: 1000 },
+    { address: 'EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow', reward: 1000 },
+    { address: 'EQBvumwjKe7xlrjc22p2eLGT4UkdRnrmqmcEYT94J6ZCINmt', reward: 1000 },
+    { address: 'EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl', reward: 1000 },
+    { address: 'EQAO7jXcX-fJJZl-kphbpdhbIDUqcAiYcAr9RvVlFl38Uatt', reward: 1000 },
+    { address: 'EQAvheS_G-U57CE55UlwF-3M-cc4cljbLireYCmAMe_RHWGF', reward: 1000 },
+    { address: 'EQCba5q9VoYGgiGykVazOUZ49UK-1RljUeZgU6E-bW0bqF2Z', reward: 1000 },
+    { address: 'EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV', reward: 1000 },
+    { address: 'EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY', reward: 1000 },
+    { address: 'EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA', reward: 1000 }, // 1000
+];
